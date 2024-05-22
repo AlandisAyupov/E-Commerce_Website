f64c8a8bdad96944c83b1a6236c3a71364e55507
AlandisAyupov <steamuservni@gmail.com>
2024-05-21 22:23:46 -0400

    Running

--- src/main/java/com/ayupov/Bank/user/UserRepository.java
+++ src/main/java/com/ayupov/Bank/user/UserRepository.java
@@ -1,6 +1,6 @@
-package com.ayupov.Bank.user;
+// package com.ayupov.Bank.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
+// public interface UserRepository extends JpaRepository<User, Integer> {
+// }
--- src/main/java/com/ayupov/Bank/user/UserService.java
+++ src/main/java/com/ayupov/Bank/user/UserService.java
@@ -1,21 +1,39 @@
 package com.ayupov.Bank.user;
 
-import java.beans.JavaBean;
 import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
 
 @Service
-public interface UserService {
-    void addUser(User user);
+public class UserService {
+    void addUser(User user)
+    {
+        return;
+    }
 
-    List<User> getUsers();
+    List<User> getUsers()
+    {
+        return new ArrayList<User>();
+    }   
 
-    User getUser(String email);
+    User getUser(String email)
+    {
+        return new User();
+    }
 
-    void updateUser(String email, User user);
+    void updateUser(String email, User user)
+    {
+        return;
+    }
 
-    void deleteUser(String email);
+    void deleteUser(String email)
+    {
+        return;
+    }
 
-    void updateName(String email, UserDTO userDTO);
+    void updateName(String email, UserDTO userDTO)
+    {
+        return;
+    }
 }
