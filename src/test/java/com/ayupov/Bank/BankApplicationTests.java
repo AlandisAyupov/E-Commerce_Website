9789af0b443f999230aa71d1c33813b74a3c3893
AlandisAyupov <steamuservni@gmail.com>
2024-06-21 22:32:35 -0400

    .

--- src/test/java/com/ayupov/Bank/BankApplicationTests.java
+++ src/test/java/com/ayupov/Bank/BankApplicationTests.java
@@ -1,7 +1,8 @@
 package com.ayupov.Bank;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,10 +11,12 @@ class BankApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Allure.step(String.format("contextLoads"));
 	}
 
 	@Test
 	public void givenNumber_whenEven_thenTrue() {
+		Allure.step("assertEven");
 		assertTrue(2 % 2 == 0);
 	}
 
