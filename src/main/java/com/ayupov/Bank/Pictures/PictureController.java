85d1cafbfdde65a536b1781b4abf9840a0617902
AlandisAyupov <steamuservni@gmail.com>
2024-11-03 21:20:51 -0500

    Picture Controller

--- src/main/java/com/ayupov/Bank/Pictures/PictureController.java
+++ src/main/java/com/ayupov/Bank/Pictures/PictureController.java
@@ -1,5 +1,35 @@
 package com.ayupov.Bank.Pictures;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ayupov.Bank.Application;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("/picture")
+@CrossOrigin(origins = { "http://localhost:4000" }, allowedHeaders = "*", allowCredentials = "true")
 public class PictureController {
     
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private PictureService pictureService;
+
+    @PostMapping("/add")
+    public String addPicture(@RequestParam("file") MultipartFile file) {
+        
+    }
 }
