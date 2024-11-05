91c3e47a1ee011d29432ce0311a56fdd5d46284a
AlandisAyupov <steamuservni@gmail.com>
2024-11-05 14:38:27 -0500

    Add Picture

--- src/main/java/com/ayupov/Bank/Pictures/PictureController.java
+++ src/main/java/com/ayupov/Bank/Pictures/PictureController.java
@@ -4,16 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ayupov.Bank.Application;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +32,19 @@ public class PictureController {
     private PictureService pictureService;
 
     @PostMapping("/add")
-    public String addPicture(@RequestParam("file") MultipartFile file) {
-        
+    public int addPicture(@RequestParam("file") MultipartFile file) {
+        byte[] fileContent;
+        try {
+            String encodedString = Base64.getEncoder().encodeToString(file.getBytes());
+            Picture picture = new Picture();
+            UUID uuid = UUID.randomUUID();
+            picture.setPictureID(uuid.toString());
+            picture.setData(encodedString);
+            picture.setEncoding("base64");
+            return pictureService.addPicture(picture);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
--- src/main/java/com/ayupov/Bank/Pictures/PictureService.java
+++ src/main/java/com/ayupov/Bank/Pictures/PictureService.java
@@ -1,5 +1,27 @@
 package com.ayupov.Bank.Pictures;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.ayupov.Bank.Application;
+import com.ayupov.Bank.Pictures.PictureRepository;
+
+@Service
 public class PictureService {
-    
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    private final PictureRepository pictureRepository;
+
+    public PictureService(PictureRepository pictureRepository)
+    {
+        // this keyword refers to current instance
+        this.pictureRepository = pictureRepository;
+    }
+
+    public int addPicture(Picture picture)
+    {
+        pictureRepository.save(picture);
+        return 200;
+    }
 }
