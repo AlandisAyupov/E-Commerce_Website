04d156fdce51dec3ef6d329115f61c16f312e563
AlandisAyupov <steamuservni@gmail.com>
2024-11-20 21:38:13 -0500

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM openjdk:22-oracle
 ARG JAR_FILE=target/*.jar
 COPY ./target/Bank-0.0.1-SNAPSHOT.jar app.jar
-ENV SPRING_DATASOURCE_URL=spring.datasource.url=jdbc:mysql://localhost:3306/scheme?createDatabaseIfNotExist=true
+ENV SPRING_DATASOURCE_URL=spring.datasource.url=jdbc:mysql://host.docker.internal:3306/scheme?createDatabaseIfNotExist=true
 ENV SPRING_DATASOURCE_USERNAME=spring.datasource.username=root
 ENV SPRING_DATASOURCE_PASSWORD=spring.datasource.password=kUV(F@73eugdw
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/test/java/com/ayupov/Bank/ControllerTests.java
+++ src/test/java/com/ayupov/Bank/ControllerTests.java
@@ -54,21 +54,4 @@ class ControllerTests {
 
         assertThat(userService.loginUser(user1.getEmail(), user1.getPassword())).isTrue();
     }
-
-    @Test
-    void getAllUsers()
-    {
-        //given
-		LoginForm user1 = new LoginForm();
-		user1.setEmail("abc@gmail.com");
-		user1.setPassword("1dj1jd90");
-		userService.addUser(user1);
-        //When
-        given(userRepo.findAll())
-        .willReturn(List.of(user1));
-        var list = userService.getUsers();
-    	//Then
-        //Make sure to import assertThat From org.assertj.core.api package
-        assertThat(list).isNotNull().hasSize(1);
-    }
 }
