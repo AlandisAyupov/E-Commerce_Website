aac1121ba126438ec9a1aadc7f2dc08dc3a5d68d
AlandisAyupov <steamuservni@gmail.com>
2025-01-30 11:01:43 -0500

    Spring profiles

--- src/main/java/com/ayupov/Bank/Application.java
+++ src/main/java/com/ayupov/Bank/Application.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import java.util.Collections;
 
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class Application {
